526ecf8670a5753dc5bfd6718eb24d7be345c5ac
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-05-02 09:41:53 -0700

    Lean Tween the Title Screen

---
+++ Assets/Scripts/TitleScreen/PanelTween.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelTween : MonoBehaviour
+{
+    void Awake()
+    {
+        LeanTween.scale(gameObject, Vector3.one, 1f).setEase(LeanTweenType.easeOutElastic).setDelay(0.25f);
+    }
+}
---
+++ Assets/Scripts/TitleScreen/TitleTween.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleTween : MonoBehaviour
+{
+    void Awake()
+    {
+        LeanTween.scale(gameObject, Vector3.one, 1f).setEase(LeanTweenType.easeOutElastic);
+    }
+}
