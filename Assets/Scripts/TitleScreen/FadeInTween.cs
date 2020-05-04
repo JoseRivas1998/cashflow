70c3ad608b3677577b6bdfff8250679e29e60d47
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2020-05-04 16:51:35 -0700

    Tile Screen Fade In

---
+++ Assets/Scripts/TitleScreen/FadeInTween.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInTween : MonoBehaviour
+{
+    public float fadeInTime = 1f;
+    public Image image;
+    public GameObject title;
+    public GameObject options;
+
+    private void Start()
+    {
+        LeanTween.value(image.gameObject, 1f, 0f, fadeInTime)
+                .setOnUpdate((val) =>
+                {
+                    Color c = image.color;
+                    c.a = val;
+                    image.color = c;
+                })
+                .setEase(LeanTweenType.easeInOutSine)
+                .setOnComplete(() =>
+                {
+                    title.SetActive(true);
+                    options.SetActive(true);
+                    Destroy(gameObject);
+                });
+    }
+
+}
